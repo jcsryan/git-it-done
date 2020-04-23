e33dabcec4decf2e68cc561ed1d20c23c1768543
JCSR <johnconnorsryan@gmail.com>
2020-04-22 22:22:35 -0400

    finished button functions

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -77,6 +77,30 @@ repoEl.appendChild(statusEl);
     repoContainerEl.appendChild(repoEl);
   }
 };
+
+var getFeaturedRepos = function(language){
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response){
+    if(response.ok) {
+      response.json().then(function(data){
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("error" + response.statusText);
+    }
+  });
+};
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language")
+  if(language){
+    getFeaturedRepos(language);
+    repoContainerEl.textContent="";
+  }
+};
+
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
